1663ac5f65b12807d9dc69286a2543f75aef6db8
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2025-02-24 23:21:10 +0600

    added: 1. flash boot img 2. confirmation before reboot after flashing recovery

--- flash.sh
+++ flash.sh
@@ -3,17 +3,18 @@ trap "echo -e '\033[0;31m Script execution aborted. \033[0m'; exit 1" INT
 
 echo -e "\033[0;33m
 What do you want to do?
-1. Flash Recovery
-2. Flash VBMETA
-3. Use adb Sideload
-4. Use adb Sideload and Reboot to recovery
-5. Check adb devices
-6. Check fastboot devices
-7. Reboot to System
-8. Reboot to Recovery
-9. Reboot to Bootloader
-10. Reboot to Fastboot
-11. Flash MIUI Stock ROM (Fastboot)
+1. Flash Boot
+2. Flash Recovery
+3. Flash VBMETA
+4. Use adb Sideload
+5. Use adb Sideload and Reboot to recovery
+6. Check adb devices
+7. Check fastboot devices
+8. Reboot to System
+9. Reboot to Recovery
+10. Reboot to Bootloader
+11. Reboot to Fastboot
+12. Flash MIUI Stock ROM (Fastboot)
 (Press Any key to Exit or input your choice.)
 \033[0m";
 
@@ -21,6 +22,65 @@ read -p "Choice: " flasher
 
 case $flasher in
   "1")
+
+    echo -e "\033[0;33m Please ensure that 'boot.img' file exist in root directory. Press enter to continue \033[0m";
+
+    read
+
+    files=("boot.img" "./platform-tools/fastboot" "./platform-tools/adb")
+
+    for file in "${files[@]}"; do
+      if [ ! -f "$file" ]; then
+        echo -e "\033[0;31m > $file file is not found \033[0m";
+      fi
+    done
+
+    for file in "${files[@]}"; do
+      if [ ! -f "$file" ]; then
+        echo -e "\033[0;31m \033[3m Please ensure that all of these files exist and try again. Have a good day! \033[0m"
+        exit 1;
+      fi
+    done
+    
+    echo -e "\033[0;32m Flashing Boot \033[0m";
+    ./platform-tools/fastboot flash boot boot.img
+
+     read -p "Do you want to flash 'recovery.img' file? Type 'y' to flash, any key to skip: " recovery;
+
+    if [ "$recovery" = "y" ] || [ "$recovery" = "yes" ] || [ "$recovery" = "Y" ] || [ "$recovery" = "YES" ]; then
+      echo -e "\033[0;33m make sure 'recovery.img' file exist in the root directory. \033[0m";
+      echo -e "\033[0;32m Flashing recovery.img \033[0m";
+      ./platform-tools/fastboot flash recovery recovery.img
+    else
+      echo -e "\033[0;32m Skipping Flashing recovery.img \033[0m";
+    fi
+    
+    read -p "Do you want to flash 'vbmeta.img' file? Type 'y' to flash, any key to skip: " vbimg;
+
+    if [ "$vbimg" = "y" ] || [ "$vbimg" = "yes" ] || [ "$vbimg" = "Y" ] || [ "$vbimg" = "YES" ]; then
+      echo -e "\033[0;33m make sure 'vbmeta.img' file exist in the root directory. \033[0m";
+      echo -e "\033[0;32m Flashing vbmeta.img \033[0m";
+      ./platform-tools/fastboot --disable-verity --disable-verification flash vbmeta vbmeta.img
+    else
+      echo -e "\033[0;32m Skipping Flashing vbmeta.img \033[0m";
+    fi
+
+    read -p "Type 'y' to reboot to recovery mode, any key to skip: " rmode;
+
+    if [ "$rmode" = "y" ]; then
+      echo -e "\033[0;32m Rebooting to Recovery. \033[0m";
+      ./platform-tools/fastboot reboot recovery
+    else
+      echo -e "\033[0;32m Skipping Rebooting to Recovery. \033[0m";
+    fi
+
+    if [ $? -eq 0 ]; then
+      echo -e "\033[0;32m Operation Succeed \033[0m";
+    fi
+
+    source ./flash.sh
+  ;;
+  "2")
 
     echo -e "\033[0;33m Please ensure that 'recovery.img' file exist in root directory. Press enter to continue \033[0m";
 
@@ -54,8 +114,14 @@ case $flasher in
       echo -e "\033[0;32m Skipping Flashing vbmeta.img \033[0m";
     fi
 
-    echo -e "\033[0;32m Rebooting to Recovery \033[0m";
-    ./platform-tools/fastboot reboot recovery
+    read -p "Type 'y' to reboot to recovery mode, any key to skip" rmode1;
+
+    if [ "$rmode1" = "y" ]; then
+      echo -e "\033[0;32m Rebooting to Recovery. \033[0m";
+      ./platform-tools/fastboot reboot recovery
+    else
+      echo -e "\033[0;32m Skipping Rebooting to Recovery. \033[0m";
+    fi
 
     if [ $? -eq 0 ]; then
       echo -e "\033[0;32m Operation Succeed \033[0m";
@@ -63,7 +129,7 @@ case $flasher in
 
     source ./flash.sh
   ;;
-  "2")
+  "3")
     if [ ! -f "vbmeta.img" ]; then
         echo -e "\033[0;31m \033[3m Please ensure that 'vbmeta.img' file exist in the root directory and try again. Have a good day! \033[0m"
         exit 1;
@@ -87,7 +153,7 @@ case $flasher in
     source ./flash.sh
 
   ;;
-  "3")
+  "4")
 
     echo -e "\033[0;32m Please enter the file name of your custom rom. \033[0m";
     read -p "Enter the zip file name: " romname
@@ -111,7 +177,7 @@ case $flasher in
     source ./flash.sh
     
   ;;
-  "4")
+  "5")
 
     echo -e "\033[0;32m Please enter the file name of your custom rom. \033[0m";
     read -p "Enter the zip file name: " romname
@@ -136,43 +202,43 @@ case $flasher in
     source ./flash.sh
     
   ;;
-  "5")
+  "6")
     echo -e "\033[0;32m adb device list: \033[0m"
     ./platform-tools/adb devices
 
     source ./flash.sh
   ;;
-  "6")
+  "7")
     echo -e "\033[0;32m fastboot device list: \033[0m"
     ./platform-tools/fastboot devices
 
     source ./flash.sh
   ;;
-  "7")
+  "8")
     echo -e "\033[0;32m Rebooting your device to system! Have a good day! \033[0m";
     ./platform-tools/fastboot reboot
 
     source ./flash.sh
   ;;
-  "8")
+  "9")
     echo -e "\033[0;32m Rebooting your device to Recovery! \033[0m";
     ./platform-tools/fastboot reboot recovery
 
     source ./flash.sh
   ;;
-  "9")
+  "10")
     echo -e "\033[0;32m Rebooting your device to Bootloader! \033[0m";
     ./platform-tools/fastboot reboot bootloader
 
     source ./flash.sh
   ;;
-  "10")
+  "11")
     echo -e "\033[0;32m Rebooting your device to Fastboot! \033[0m";
     ./platform-tools/fastboot reboot fastboot
 
     source ./flash.sh
   ;;
-  "11")
+  "12")
     echo -e "\033[0;32m Great Choice! Heading up to Stock ROM Flash system! \033[0m";
     source ./flash-stock-rom.sh;
   ;;
